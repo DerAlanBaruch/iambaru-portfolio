03864f1085042ed58c8c1f12d21571e157d22d52
baruchalan3 <baruchalan3@gmail.com>
2024-03-13 03:24:30 +0000

    Config for firebase deploying

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,15 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+  // {
+  //   path: '',
+  //   loadComponent: () =>
+  //     import('./components/pages/home-page/home-page.component').then(
+  //       (c) => c.HomePageComponent
+  //     ),
+  // },
   {
     path: '',
-    loadComponent: () =>
-      import('./components/pages/home-page/home-page.component').then(
-        (c) => c.HomePageComponent
-      ),
-  },
-  {
-    path: 'links',
     loadComponent: () =>
       import('./components/pages/links-page/links-page.component').then(
         (c) => c.LinksPageComponent
